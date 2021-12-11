c97cc7126d2083961db39526d850e74f34d726bb
jsakamoto <junichi.jp@gmail.com>
2021-12-11 15:57:27 +0900

    Initial commit

---
+++ README.md
@@ -0,0 +1,14 @@
+﻿# Toolbelt.WorkDirectory [![NuGet Package](https://img.shields.io/nuget/v/Toolbelt.WorkDirectory.svg)](https://www.nuget.org/packages/Toolbelt.WorkDirectory/)
+
+## Summary
+
+Create a temporary working directory, copy files from a template, and finally, delete the working directory altogether when exiting a scope.
+
+
+## Release Notes
+
+[Release Notes](https://github.com/jsakamoto/Toolbelt.WorkDirectory/blob/master/RELEASE-NOTES.txt)
+
+## License
+
+[Mozilla Public License Version 2.0](https://github.com/jsakamoto/Toolbelt.WorkDirectory/blob/master/LICENSE)
---
+++ Toolbelt.WorkDirectory/WorkDirectory.cs
@@ -0,0 +1,37 @@
+﻿namespace Toolbelt;
+
+public class WorkDirectory : IDisposable
+{
+    public string Path { get; }
+
+    public WorkDirectory() : this(AppDomain.CurrentDomain.BaseDirectory)
+    {
+    }
+
+    public WorkDirectory(string baseDir)
+    {
+        this.Path = System.IO.Path.Combine(baseDir, Guid.NewGuid().ToString("N"));
+        Directory.CreateDirectory(this.Path);
+    }
+
+    private void CopyFrom(string srcDir, Func<(string Dir, string NAme), bool>? predicate)
+    {
+        FileIO.XcopyDir(srcDir, dstDir: this.Path, predicate);
+    }
+
+    public void Dispose()
+    {
+        try { FileIO.DeleteDir(this.Path); } catch { }
+    }
+
+    public override string ToString() => this.Path;
+
+    public static implicit operator string(WorkDirectory workDirectory) => workDirectory.Path;
+
+    public static WorkDirectory CreateCopyFrom(string srcDir, Func<(string Dir, string Name), bool>? predicate)
+    {
+        var workDir = new WorkDirectory();
+        workDir.CopyFrom(srcDir, predicate);
+        return workDir;
+    }
+}
